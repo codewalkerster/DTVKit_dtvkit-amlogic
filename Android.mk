6e79b813143cabdbdfa40a5a0ed559ab875148d6
junchao yuan <junchao.yuan@amlogic.com>
2018-11-26 14:45:32 +0800

    dtvkit-amlogic: move library from system to vendor [1/1]
    PD#SWPL-2256
    
    Problem:
    need move rpcservice to vendor in android p version;
    
    Solution:
    add LOCAL_VENDOR_MODULE := true in android.mk;
    
    Verify:
    verified by R311
    
    Change-Id: Id266c368293efe75d972e483cf08fb370406a29e
    Signed-off-by: junchao yuan <junchao.yuan@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 ifeq ($(TARGET_ARCH),"arm")
-	DTVKIT_USE_STDINT = 0
+    DTVKIT_USE_STDINT = 0
 else
-	DTVKIT_USE_STDINT = 1
+    DTVKIT_USE_STDINT = 1
 endif
 
 DTVKIT_OPTIMISATION_OPTION?=-O2
@@ -13,63 +13,63 @@ LOCAL_MODULE := libdtvkit_platform
 LOCAL_MODULE_TAGS := optional
 
 ifneq ($(DTVKIT_USE_STDINT),1)
-	LOCAL_CFLAGS += -DNO_STDINT_H
+    LOCAL_CFLAGS += -DNO_STDINT_H
 endif
 ifeq ($(DTVKIT_COLOUR_DEPTH),8)
-	LOCAL_CFLAGS += -DOSD_8_BIT
+    LOCAL_CFLAGS += -DOSD_8_BIT
 else
-	ifeq ($(DTVKIT_COLOUR_DEPTH),16)
-		LOCAL_CFLAGS += -DOSD_16_BIT
-	else
-		DTVKIT_COLOUR_DEPTH=32
-		LOCAL_CFLAGS += -DOSD_32_BIT
-		ifeq ($(DTVKIT_OSD_ST_MODE), 1)
-			DTVKIT_COLOUR_DEPTH=31
-			LOCAL_CFLAGS += -DOSD_ST_MODE
-		endif
-	endif
+    ifeq ($(DTVKIT_COLOUR_DEPTH),16)
+        LOCAL_CFLAGS += -DOSD_16_BIT
+    else
+        DTVKIT_COLOUR_DEPTH=32
+        LOCAL_CFLAGS += -DOSD_32_BIT
+        ifeq ($(DTVKIT_OSD_ST_MODE), 1)
+            DTVKIT_COLOUR_DEPTH=31
+            LOCAL_CFLAGS += -DOSD_ST_MODE
+        endif
+    endif
 endif
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../DVBCore/inc \
-	$(LOCAL_PATH)/../DVBCore/platform/inc \
-	$(LOCAL_PATH)/../AMLogic_CI-Plus/include \
-	$(LOCAL_PATH)/hw/inc \
-	$(LOCAL_PATH)os/inc \
-	vendor/amlogic/common/external/dvb/android/ndk/include \
-	vendor/amlogic/common/external/dvb/include \
-	vendor/amlogic/common/external/dvb/android/ndk/include/linux \
-	vendor/amlogic/common/external/dvb/android/ndk/include \
-	bionic/libc/kernel/uapi \
-	bionic/libc/kernel/android/uapi \
-	bionic/libc/stdio \
-	bionic/libc/include \
-	bionic/libc/../libm/include
+    $(LOCAL_PATH)/../DVBCore/platform/inc \
+    $(LOCAL_PATH)/../CI-Plus/include \
+    $(LOCAL_PATH)/hw/inc \
+    $(LOCAL_PATH)os/inc \
+    vendor/amlogic/common/external/dvb/android/ndk/include \
+    vendor/amlogic/common/external/dvb/include \
+    vendor/amlogic/common/external/dvb/android/ndk/include \
+    bionic/libc/kernel/uapi \
+    bionic/libc/kernel/android/uapi \
+    bionic/libc/stdio \
+    bionic/libc/include \
+    bionic/libc/../libm/include \
+    hardware/amlogic/media/amcodec/include\
 
 ifeq ($(TARGET_ARCH),"arm")
-	ANDROID_HEADERS+=" -I${BIONIC_LIB}/arch-arm/include"
-	ANDROID_HEADERS+=" -I${BIONIC_LIB}/kernel/uapi/asm-arm"
+    ANDROID_HEADERS+=" -I${BIONIC_LIB}/arch-arm/include"
+    ANDROID_HEADERS+=" -I${BIONIC_LIB}/kernel/uapi/asm-arm"
 else
-	ANDROID_HEADERS+=" -I${BIONIC_LIB}/arch-arm64/include"
-	ANDROID_HEADERS+=" -I${BIONIC_LIB}/kernel/uapi/asm-arm64"
+    ANDROID_HEADERS+=" -I${BIONIC_LIB}/arch-arm64/include"
+    ANDROID_HEADERS+=" -I${BIONIC_LIB}/kernel/uapi/asm-arm64"
 endif
 
 LOCAL_SRC_FILES := hw/src/stbhwplatform.c \
-	hw/src/stbhwini.c \
-	hw/src/stbhwmem.c \
-	hw/src/stbhwtun.c \
-	hw/src/stbhwdmx.c \
-	hw/src/stbhwav.c  \
-	hw/src/stbhwdsk.c \
-	hw/src/stbhwfp.c  \
-	hw/src/stbhwsp.c  \
-	hw/src/stbhwcrypt.c \
-	hw/src/stbhwupg.c \
-	hw/src/stbhwci.c \
-	hw/src/stbhwosd.c \
-	hw/src/stbpvrpr.c \
-	hw/src/stbhwnet.c \
-	hw/src/stbhwvbi.c \
-	os/src/stbos_event.c      \
+    hw/src/stbhwini.c \
+    hw/src/stbhwmem.c \
+    hw/src/stbhwtun.c \
+    hw/src/stbhwdmx.c \
+    hw/src/stbhwav.c  \
+    hw/src/stbhwdsk.c \
+    hw/src/stbhwfp.c  \
+    hw/src/stbhwsp.c  \
+    hw/src/stbhwcrypt.c \
+    hw/src/stbhwupg.c \
+    hw/src/stbhwci.c \
+    hw/src/stbhwosd.c \
+    hw/src/stbpvrpr.c \
+    hw/src/stbhwnet.c \
+    hw/src/stbhwvbi.c \
+    os/src/stbos_event.c      \
     os/src/stbos_mutex.c      \
     os/src/stbos_queue.c      \
     os/src/stbos_rtc.c        \
@@ -80,5 +80,7 @@ LOCAL_CFLAGS+=-DANDROID $(DTVKIT_OPTIMISATION_OPTION)
 LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_STATIC_LIBRARIES+=libcutils
+
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_STATIC_LIBRARY)

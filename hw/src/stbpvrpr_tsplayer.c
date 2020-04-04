3b3d346d9b8867d1d46ad2468cd7b448ab8dc7aa
Zhiqiang Han <zhiqiang.han@amlogic.com>
2020-04-04 12:10:00 +0800

    pvr: ff/fb fail in pause mode [1/1]
    PD#SWPL-23654
    
    Problem:
    incorrect behavior of ff/fb
    
    Solution:
    libdvr dost not support pause->ff/fb
    shoule insert resume action.
    
    Verify:
    Patchbuild
    
    Change-Id: Ibe5284c896ab29e442cfa9f5944566f3160fba73
    Signed-off-by: Zhiqiang Han <zhiqiang.han@amlogic.com>

--- hw/src/stbpvrpr_tsplayer.c
+++ hw/src/stbpvrpr_tsplayer.c
@@ -1281,6 +1281,8 @@ BOOLEAN STB_PVRSetPlaySpeed(U8BIT audio_decoder, U8BIT video_decoder, S16BIT spe
          }
          else if (check_speed_ok(speed))
          {
+            if (s_recplay_status[play_index].play_speed == 0)
+               error = dvr_wrapper_resume_playback(s_recplay_status[play_index].player);
             error = dvr_wrapper_set_playback_speed(s_recplay_status[play_index].player, speed);
          }
          else
@@ -1291,7 +1293,8 @@ BOOLEAN STB_PVRSetPlaySpeed(U8BIT audio_decoder, U8BIT video_decoder, S16BIT spe
 
          if (!error)
          {
-            PLAY_DBG("Set play speed to %d%%", speed);
+            PLAY_DBG("Set play speed to %d%% -> %d%%",
+               s_recplay_status[play_index].play_speed, speed);
             s_recplay_status[play_index].play_speed = speed;
             retval = TRUE;
          }

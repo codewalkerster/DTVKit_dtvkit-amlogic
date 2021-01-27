ae6ce5d1343208c0a52b60b1a4cf7f068eaccdb9
jianchuan.ping <jianchuan.ping@amlogic.com>
2021-01-27 16:36:17 +0800

    Dtvkit_amlogic: add support for get video info. [1/1]
    PD#SWPL-42171
    
    Problem:
    Need get frame rate and scan type.
    
    Solution:
    Add support for this info
    
    Verify:
    AH212
    
    Change-Id: Ied5712fc706d2d0a0811e57b4c9247300f7bce55
    Signed-off-by: jianchuan.ping <jianchuan.ping@amlogic.com>

--- hw/src/stbhwav_amlmp.c
+++ hw/src/stbhwav_amlmp.c
@@ -2565,7 +2565,6 @@ void STB_AVSetSpdifMode(U8BIT path, E_STB_DIGITAL_AUDIO_TYPE audio_type)
 void STB_AVGetScreenSize(U8BIT path, U16BIT *width, U16BIT *height)
 {
    FUNCTION_START(STB_AVGetScreenSize);
-   USE_UNWANTED_PARAM(path);
    U8BIT av_path = STB_AVGetPath(path, INVALID_RES_ID);
 
    VID_DBG("video codec path=%u av_path = %u", path, av_path);
@@ -2638,13 +2637,62 @@ E_STB_AV_SRM_REPLY STB_AVApplySRM(U8BIT path, U8BIT *data, U32BIT len)
  */
 U8BIT STB_AVGetVideoFrameRate(U8BIT path)
 {
-   FUNCTION_START(STB_AVGetVideoFrameRate);
-   USE_UNWANTED_PARAM(path);
-   FUNCTION_FINISH(STB_AVGetVideoFrameRate);
-   return 0;
+    int ret;
+    AML_MP_PLAYER handle;
+    U8BIT frame_rate = 0;
+    FUNCTION_START(STB_AVGetVideoFrameRate);
+    VID_DBG("vpath:%u", path);
+
+    ret = AV_GetPlayerHandleByPath(path, INVALID_RES_ID, &handle, FALSE);
+    if (ret == 0)
+    {
+        Aml_MP_VideoInfo info;
+
+        ret = Aml_MP_Player_GetParameter(handle, AML_MP_PLAYER_PARAMETER_VIDEO_INFO, &info);
+        if (ret == 0)
+        {
+            frame_rate = (U8BIT)info.frameRate;
+        }
+    }
+    FUNCTION_FINISH(STB_AVGetVideoFrameRate);
+    return frame_rate;
 }
 
+/**
+ * @brief   Returns the scan type of the video being decoded
+ * @param   path video path
+ * @return  1: progressive, 0: interlaced, 255: invalid
+ */
+U8BIT STB_AVGetVideoScanType(U8BIT path)
+{
+    int ret;
+    AML_MP_PLAYER handle;
+    U8BIT scan_type = 0;
 
+    FUNCTION_START(STB_AVGetVideoScanType);
+
+    VID_DBG("vpath:%u", path);
+
+    ret = AV_GetPlayerHandleByPath(path, INVALID_RES_ID, &handle, FALSE);
+    if (ret == 0)
+    {
+        Aml_MP_VdecStat info;
+
+        ret = Aml_MP_Player_GetParameter(handle, AML_MP_PLAYER_PARAMETER_VIDEO_DECODE_STAT, &info);
+        if (ret == 0)
+        {
+            if ((info.vf_type & 0x01) == 0x01 ||
+                (info.vf_type & 0x03) == 0x03 ||
+                (info.vf_type & 0x08) == 0x08)
+                scan_type = 0;
+            else
+                scan_type = 1;
+        }
+    }
+
+    FUNCTION_FINISH(STB_AVGetVideoScanType);
+    return scan_type;
+}
 
 /**
  * @brief Apply the specified copy protection. This function is used for CI+

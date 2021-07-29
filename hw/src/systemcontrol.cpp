686fc23f8347c963be1cb18bd7f8556cc0e76387
hujian.zheng <hujian.zheng@amlogic.com>
2021-07-29 19:23:06 +0800

    dtvkit_amlogic: add Mute AV features [1/1]
    PD#TV-37743
    
    Problem:
    change follow
    
    Solution:
    change follow
    
    Verify:
    T5D
    
    Change-Id: I9560855f92b8e578f02c037290752ca4bff7dec1
    Signed-off-by: hujian.zheng <hujian.zheng@amlogic.com>

---
+++ hw/src/systemcontrol.cpp
@@ -0,0 +1,78 @@
+
+#include <utils/threads.h>
+#include <SystemControlClient.h>
+#include <android/log.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Ocean Blue Software header files
+#include "techtype.h"
+#include "dbgfuncs.h"
+#include "systemcontrol.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#define SCDBG(x,...)	STB_SPDebugWrite("%s:%d " x,__FUNCTION__,__LINE__, ##__VA_ARGS__ )
+
+using namespace android;
+/*-----------------------------------------------------------------------------*/
+/* Local Constant Definitions                                                  */
+/*-----------------------------------------------------------------------------*/
+#define LOG_TAG "tvserver"
+
+static Mutex amLock;
+static sp<SystemControlClient> sysctrlClient = nullptr;
+/*-----------------------------------------------------------------------------*/
+/* Local Types Declarations                                                    */
+/*-----------------------------------------------------------------------------*/
+static const sp<SystemControlClient> &getSystemControlService()
+{
+    Mutex::Autolock _l(amLock);
+    if (sysctrlClient == nullptr) {
+        sysctrlClient = SystemControlClient::getInstance();
+    }
+    //ALOGE_IF(sysctrlClient == nullptr, "no System Control Service!?")
+
+    return sysctrlClient;
+}
+
+extern "C"  int SC_setVideoColor(int color)
+{
+#if ANDROID_PLATFORM_SDK_VERSION >= 30
+    int s32Ret = -1;
+    const sp<SystemControlClient> &sws = getSystemControlService();
+    if (sws != nullptr) {
+
+        if(color == VIDEO_LAYER_COLOR_MAX)
+            SCDBG("@@@@@@@@@@@@@ UNMUTE");
+        else
+            SCDBG("@@@@@@@@@@@@@ MUTE [%d]", color);
+
+        s32Ret = sws->setVideoScreenColor(color);
+        return s32Ret;
+    }
+#endif
+    return -1;
+}
+
+extern "C"  int SC_getScreenColorSetting()
+{
+#if ANDROID_PLATFORM_SDK_VERSION >= 30
+    int s32Ret = -1;
+    const sp<SystemControlClient> &sws = getSystemControlService();
+    if (sws != nullptr) {
+        s32Ret = sws->getScreenColorForSignalChange();
+        //s32Ret = VIDEO_LAYER_COLOR_BLUE;
+        return s32Ret;
+    }
+#endif
+    return -1;
+}
+
+
+/*****************************************************************************
+*                    End Of File
+*****************************************************************************/

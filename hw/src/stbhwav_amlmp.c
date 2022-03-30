2aead58be5e7193de4dc1722a27d2b5203a4f8d0
Yan Yan <yan.yan@amlogic.com>
2022-03-30 10:56:52 +0800

    AV: add av control flag [5/5]
    PD#SWPL-75794
    
    Problem:
    change follow
    
    Solution:
    change follow
    
    Verify:
    T5D
    
    Change-Id: I905b3a5289bd12e15a34cb72c1ff50e5fe8e8de0
    Signed-off-by: Yan Yan <yan.yan@amlogic.com>

--- hw/src/stbhwav_amlmp.c
+++ hw/src/stbhwav_amlmp.c
@@ -833,13 +833,13 @@ U8BIT STB_AVGetAudioVolume(U8BIT path)
    return vol;
 }
 
-void STB_ResetVideoBlank(path)
+void STB_ResetVideoBlank(U8BIT path)
 {
    av_paths_status[path].video_out_control = 0;
    STB_AVBlankVideo(path, 0, FALSE);
 }
 
-void STB_ResetAudioMute(path)
+void STB_ResetAudioMute(U8BIT path)
 {
    av_paths_status[path].audio_out_control = 0;
    STB_AVSetAudioMute(path, 0, FALSE);

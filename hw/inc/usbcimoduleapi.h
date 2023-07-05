a838162e3ed00c52e56dbabafb6fce25be2dbfa0
guoqing.sun <guoqing.sun@amlogic.com>
2023-07-05 11:20:40 +0800

    CI: CB2 usbcam dtvkit. [1/1]
    PD#SWPL-106076
    
    Problem:
    access the usbcam driver for commands and TS data.
    
    Solution:
    access the usbcam driver for commands and TS data.
    
    Verify:
    T3 android-r
    
    Change-Id: If68961dc65d640d8f88d84ccebe23e5d769f03e2
    Signed-off-by: guoqing.sun <guoqing.sun@amlogic.com>

--- hw/inc/usbcimoduleapi.h
+++ hw/inc/usbcimoduleapi.h
@@ -1,8 +1,9 @@
 #ifndef __USBCIMODULEAPI_H
 #define __USBCIMODULEAPI_H
 #include <stdint.h>
+#include <stdbool.h>
 #define USB_CIMODULE_COMMAND_MAX_SIZE    0x2000
-#define USB_CIMODULE_MEDIA_MAX_SIZE      (47 * 1024)
+#define USB_CIMODULE_MEDIA_MAX_SIZE      (47 * 1024 )
 #define ERROR_USB_REMOVE			-19
 #define ERROR_INVALID_ARGUMENT		-22
 #define ERROR_PROTOCOL_ERROR		-71
@@ -14,26 +15,46 @@ struct usb_cimodule_info
    unsigned short m_wVendorId;
    unsigned short m_wProductId;
    uint32_t m_dwCiCompatibility;
-   unsigned char m_bIsCI20Deteced;
+   unsigned char m_bIsCI20Detected;
    unsigned char m_arReserve[31];
 };
-int cimodule_cmd_intf_open(const char *i_pbDevpath, int i_nflags);
-int cimodule_cmd_intf_close(int i_fd);
-unsigned char *cimodule_cmd_intf_mmap_readbuf(int i_fd, unsigned int i_dwMaxCmdBufReadSize);
-unsigned char *cimodule_cmd_intf_mmap_writebuf(int i_fd, unsigned int i_dwMaxCmdBufWriteSize);
-void cimodule_cmd_intf_munmap_readbuf(unsigned char *i_pbBuf);
-void cimodule_cmd_intf_munmap_writebuf(unsigned char *i_pbBuf);
-int cimodule_cmd_intf_read(int i_fd, unsigned char *o_pbBuf, unsigned int i_dwRqstLen, unsigned int *o_pdwActualLen, unsigned int i_dwTimeout);
-int cimodule_cmd_intf_write(int i_fd, unsigned char *i_pbBuf, unsigned int i_dwRqstLen, unsigned int *o_pdwActualLen, unsigned int i_dwTimeout);
-int cimodule_media_intf_open(const char *i_pbDevpath, int i_nflags);
-int cimodule_media_intf_close(int i_fd);
-unsigned char *cimodule_media_intf_mmap_readbuf(int i_fd, unsigned int i_dwMaxMediaBufReadSize);
-unsigned char *cimodule_media_intf_mmap_writebuf(int i_fd, unsigned int i_dwMaxMediaBufWriteSize);
-void cimodule_media_intf_munmap_readbuf(unsigned char *i_pbBuf);
-void cimodule_media_intf_munmap_writebuf(unsigned char *i_pbBuf);
-int cimodule_media_intf_read(int i_fd, unsigned char *o_pbBuf, unsigned int i_dwRqstLen, unsigned int *o_pdwActualLen, unsigned int i_dwTimeout);
-int cimodule_media_intf_write(int i_fd, unsigned char *i_pbBuf, unsigned int i_dwRqstLen, unsigned int *o_pdwActualLen, unsigned int i_dwTimeout);
-int cimodule_get_driver_version(int i_fd, unsigned int *o_pdwDriverVersion);
-int cimodule_get_usb_cimodule_info(int i_fd, struct usb_cimodule_info *o_ptUsbCiModuleInfo);
-int cimodule_reset(int i_fd);
+
+#define USBCAM_MAX_NAME_LEN 30
+typedef struct
+{
+  char   ci_manufacturer_name[USBCAM_MAX_NAME_LEN];
+  char   ci_product_name[USBCAM_MAX_NAME_LEN];
+  bool ci_plus_supported;
+  bool op_profile_supported;
+} usbci_module_capabilities_t;
+
+//ci module ioctl command
+#define AML_USBCAM_IOC_MAGIC						'c'
+#define AML_USBCAM_IOC_GET_DRIVER_VERSION			_IOR(AML_USBCAM_IOC_MAGIC, 0, uint32_t)
+#define AML_USBCAM_IOC_GET_INFO					_IOR(AML_USBCAM_IOC_MAGIC, 1, struct usb_cimodule_info)
+#define AML_USBCAM_IOC_RESET						_IO(AML_USBCAM_IOC_MAGIC, 2)
+#define AML_USBCAM_IOC_CANCEL_TRANSFER				_IO(AML_USBCAM_IOC_MAGIC, 3)
+#define AML_USBCAM_IOC_MODULE_CAPABILITIES			_IOR(AML_USBCAM_IOC_MAGIC, 4, usbci_module_capabilities_t)
+
+// int cimodule_cmd_intf_open(const char *dev_path, int flag);
+// int cimodule_cmd_intf_close(int fd);
+// unsigned char *cimodule_cmd_intf_readbuf(int fd, unsigned int cmd_readbuf_size);
+// unsigned char *cimodule_cmd_intf_writebuf(int fd, unsigned int cmd_writebuf_size);
+// void cimodule_cmd_intf_free_readbuf(unsigned char *buf);
+// void cimodule_cmd_intf_free_writebuf(unsigned char *buf);
+// int cimodule_cmd_intf_read(int fd, unsigned char *buf, unsigned int len, unsigned int *actual_len, unsigned int timeout);
+// int cimodule_cmd_intf_write(int fd, unsigned char *buf, unsigned int len, unsigned int *actual_len, unsigned int timeout);
+// int cimodule_media_intf_open(const char *dev_path, int flag);
+// int cimodule_media_intf_close(int fd);
+// unsigned char *cimodule_media_intf_readbuf(int fd, unsigned int media_readbuf_size);
+// unsigned char *cimodule_media_intf_writebuf(int fd, unsigned int media_writebuf_size);
+// void cimodule_media_intf_free_readbuf(unsigned char *buf);
+// void cimodule_media_intf_free_writebuf(unsigned char *buf);
+// int cimodule_media_intf_read(int fd, unsigned char *buf, unsigned int len, unsigned int *actual_len, unsigned int timeout);
+// int cimodule_media_intf_write(int fd, unsigned char *buf, unsigned int len, unsigned int *actual_len, unsigned int timeout);
+// int cimodule_get_driver_version(int fd, unsigned int *driver_version);
+// int cimodule_get_usb_cimodule_info(int fd, struct usb_cimodule_info *usbcam_info);
+// int aml_get_usbcam_module_capabilities(int fd, usbci_module_capabilities_t *usb_cimodule_info);
+// int cimodule_reset(int fd);
+
 #endif

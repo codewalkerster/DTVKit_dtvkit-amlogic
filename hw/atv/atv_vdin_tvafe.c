421761794aa6c20e70ee89aebdf462cf00d96d85
huimin.wang <huimin.wang@amlogic.com>
2023-09-06 11:05:32 +0800

    dtvkit-amlogic: SC_setDisplayMode add issave param [1/1]
    PD#TV-90210
    PD#TV-89884
    PD#TV-90015
    
    Problem:
    SC_setDisplayMode add issave param
    
    Solution:
    fix
    
    Verify:
    T5D
    
    Change-Id: I3c16b6db9d59f15458015f39f27712750589f3c2
    Signed-off-by: huimin.wang <huimin.wang@amlogic.com>

--- hw/atv/atv_vdin_tvafe.c
+++ hw/atv/atv_vdin_tvafe.c
@@ -379,7 +379,7 @@ int vdin_signal_handle()
 
     if (m_cur_sig_info.status == TVIN_SIG_STATUS_STABLE ) {
         if (mSetPQmode) {
-            SC_setDisplayMode(SC_getDisplayMode());
+            SC_setDisplayMode(SC_getDisplayMode(), 0);
             mSetPQmode = 0;
         }
         SC_setATVVideoColor(0, 0, 5);
@@ -400,7 +400,7 @@ int vdin_signal_handle()
         ret = stop_vdin_dec();
     } else if (m_cur_sig_info.status == TVIN_SIG_STATUS_NOSIG ) {
         if (5 != SC_getDisplayMode()) {//5:VPP_DISPLAY_MODE_FULL
-            SC_setDisplayMode(5);//no sig need full screen
+            SC_setDisplayMode(5, 0);//no sig need full screen
             mSetPQmode = 1;
         }
         SC_setATVVideoColor(0, 0, 5);

dd74fe4ead7865fb81d2f0c35a3f0b8cb96a0f4e
huimin.wang <huimin.wang@amlogic.com>
2024-01-04 07:56:59 +0000

    dtvkit-amlogic: set minimum frequency [1/1]
    PD#SWPL-152197
    
    Problem:
    ATF TUNE_REPLY not send
    
    Solution:
    set minimum frequency
    
    Verify:
    T5D
    
    Change-Id: Ic94d0de017ee647ac9f6515cbfb530ef7b5c8048
    Signed-off-by: huimin.wang <huimin.wang@amlogic.com>

--- hw/src/afc/stbhwtun_afc.c
+++ hw/src/afc/stbhwtun_afc.c
@@ -295,7 +295,7 @@ U32BIT STB_TuneGetMinTunerFreqKHz(U8BIT path)
     FUNCTION_START(STB_TuneGetMinTunerFreqKHz);
 
     //min_freq = Wrapper_TuneGetMinTunerFreqKHz(path);
-    min_freq = 0;
+    min_freq = 40000;
 
     FUNCTION_FINISH(STB_TuneGetMinTunerFreqKHz);
 
@@ -314,7 +314,7 @@ U32BIT STB_TuneGetMaxTunerFreqKHz(U8BIT path)
     FUNCTION_START(STB_TuneGetMaxTunerFreqKHz);
 
     //max_freq = Wrapper_TuneGetMaxTunerFreqKHz(path);
-    max_freq = 0x0FFFFFFF;
+    max_freq = 1002000;
 
     FUNCTION_FINISH(STB_TuneGetMaxTunerFreqKHz);
 
